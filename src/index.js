a825deb402361e66a9bae5aea4280e0001e11f2f
Charles Lao <charles.h.lao@gmail.com>
2023-04-04 15:31:46 +0800

    Add asset management for css and images

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+
+// import myName from './myName'; 
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    // // use myname function
+    // element.textContent = myName('Cody');
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
